dc363981ec3758f17d5feccb35f79594f311c319
rgcastrof <rogeriogirao1@proton.me>
2025-08-28 13:22:50 -0300

    add function to handle args

--- cmd/main.go
+++ cmd/main.go
@@ -24,43 +24,51 @@ func printHelp() {
 `, os.Args[0])
 }
 
+func handleArgs(m *tasks.Manager) {
+	cmd := os.Args[1]
+	switch cmd {
+	case "add":
+		err := m.AddTask(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+		}
+	case "list":
+		m.ListTasks()
+	case "done":
+		taskID, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = m.MarkDone(taskID)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	case "del":
+		taskID, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = m.DelTask(taskID)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	case "version":
+		fmt.Printf("%s version: %s\n", os.Args[0], version)
+	case "help":
+		printHelp()
+		return
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		printHelp()
 	} else {
 		m := tasks.NewManager()
-		cmd := os.Args[1]
-		switch cmd {
-		case "add":
-			err := m.AddTask(os.Args[2])
-			if err != nil {
-				fmt.Println(err)
-			}
-		case "list":
-			m.ListTasks()
-		case "done":
-			taskID, err := strconv.Atoi(os.Args[2])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			err = m.MarkDone(taskID)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		case "del":
-			taskID, err := strconv.Atoi(os.Args[2])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			err = m.DelTask(taskID)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-
+		handleArgs(m)
 	}
 }
