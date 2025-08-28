0b301a918ff015a0f7a7c6b8b55099755c048ee9
rgcastrof <rogeriogirao1@proton.me>
2025-08-28 11:14:23 -0300

    create todo config dir

--- storage/file.go
+++ storage/file.go
@@ -3,7 +3,21 @@ package storage
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
+
+func createDir() (string, error) {
+	confPath, err := os.UserConfigDir()
+	if err != nil {
+		return "", fmt.Errorf("Failed to read config dir: %w", err)
+	}
+	todoPath := filepath.Join(confPath, "todo")
+	err = os.MkdirAll(todoPath, 0750)
+	if err != nil {
+		return "", fmt.Errorf("Failed to create todo config dir: %w", err)
+	}
+	return todoPath, nil
+}
 
 func SaveTasks(data []byte) error {
 	file, err := os.Create("tasks.json")
